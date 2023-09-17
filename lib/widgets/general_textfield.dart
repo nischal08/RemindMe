5574a65865a852321e2a393e0c632f18a68d1405
nischal08 <nischal.karanjit@gmail.com>
2023-09-17 11:47:20 +0545

    feat: implemented edit reminder and fixed textfeild to take emoji

--- lib/widgets/general_textfield.dart
+++ lib/widgets/general_textfield.dart
@@ -117,7 +117,7 @@ class _GeneralTextFieldState extends State<GeneralTextField> {
         onTap: widget.onTap,
         autofillHints: widget.autoFillHints,
         inputFormatters: [
-          LengthLimitingTextInputFormatter(widget.maxLength),
+          LengthLimitingTextInputFormatter(widget.maxLength ),
           // FilteringTextInputFormatter.deny(
           //   RegExp(
           //       r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])'),
